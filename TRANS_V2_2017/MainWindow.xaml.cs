c2cd8d85ba762c53ad245349ab65fd8852d0bcd8
Kragnvagn <ryan.lopez@salgroupco.com>
2023-06-15 08:20:08 +0800

    Entrance
    Entrance

---
+++ TRANS_V2_2017/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
+namespace TRANS_V2_2017
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        void onClose(object sender, MouseEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        //private void LoginGrid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    this.MouseLeftButtonDown += delegate { DragMove(); };
+        //}
+    }
+
+
+
+}
